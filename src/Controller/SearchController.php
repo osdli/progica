9dc74e472785c5b3783e8311a65b116e84fd04ad
MelCCode <90129638+MelCCode@users.noreply.github.com>
2022-03-30 11:12:44 +0200

    Update SearchController.php

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -6,13 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
+
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
-    public function index(): Response
+    /**
+     * @Route("/search", name="app_search")
+     */
+    public function search(Request $request)
     {
         return $this->render('search/index.html.twig', [
             'controller_name' => 'SearchController',
         ]);
     }
 }
+/*
+class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function index(Request $request): Request
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+}
+*/

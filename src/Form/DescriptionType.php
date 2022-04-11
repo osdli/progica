24d9e079daf33e0a7f10766474da2b1735c4e37a
MelCCode <90129638+MelCCode@users.noreply.github.com>
2022-04-11 13:44:55 +0200

    Update DescriptionType.php

--- src/Form/DescriptionType.php
+++ src/Form/DescriptionType.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Form;
 
 use App\Entity\Lodge;
@@ -8,7 +7,6 @@ use App\Repository\CitiesRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
@@ -16,6 +14,10 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+//use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints\File;
+
+
 
 class DescriptionType extends AbstractType
 {
@@ -35,14 +37,14 @@ class DescriptionType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Merci d\'ajouter ici la description de votre bien']
             ])
-            ->add('picture', FileType::class,[
-
-                'required' => false,
-             ])
             ->add('animal', CheckboxType::class,
-            ['label'=> 'Si  vous autorisez les animaux veuillez cocher cette case'])
+            [
+                'label'=> 'Si  vous autorisez les animaux veuillez cocher cette case',
+                'required' => false,
+            ])
             ->add('animal_cost', MoneyType::class, [
-                'label'=>'Supplément de paiement en cas de possession d\'animaux'
+                'label'=>'Supplément de paiement en cas de possession d\'animaux',
+                'required' => false,
             ])
             ->add('high_season_price', MoneyType::class, [
                 'label'=>'Prix saison haute'
@@ -50,14 +52,23 @@ class DescriptionType extends AbstractType
             ->add('low_season_price', MoneyType::class, [
                 'label'=>'Prix saison basse'
             ])
-           ->add('location', EntityType::class,[
-                'class' => Cities::class,
-                'query_builder' => function (CitiesRepository $er) {
-                return $er ->createQueryBuilder('c')
-                    ->orderBy('c.name', 'ASC')->setMaxResults(50);
-                },
-                'choice_label' => 'name',
+            
+            ->add('location', TextType::class, [
+                'label' => 'L\'emplacement de votre bien',
+
+                'attr' => [
+                    'placeholder' => 'Merci de rajouter l\'emplacement de votre bien ']
             ])
+//-------------
+        //    ->add('location', EntityType::class,[
+        //         'class' => Cities::class,
+        //         'query_builder' => function (CitiesRepository $er) {
+        //         return $er ->createQueryBuilder('c')
+        //             ->orderBy('c.name', 'ASC')->setMaxResults(50);
+        //         },
+        //         'choice_label' => 'name',
+        //     ])
+//-------------
             ->add('area',TextType::class, [
                 'label' => 'La surface de votre résidence',
 
@@ -76,10 +87,27 @@ class DescriptionType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Merci de saisir nombre de chambres à coucher']
             ])
+            ->add('picture', FileType::class,[
+                'label' => 'Image de votre bien',
+                // 'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        // 'minPixels' => '720',
+                        // 'maxPixels' => '1080',
+                        'mimeTypes' => [
+                            'image/jp2',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Merci de télécharger une photo au format .png, .jpg ou .jpeg',
+                    ])
+                ],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => "Publier"
             ])
-        ;
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
